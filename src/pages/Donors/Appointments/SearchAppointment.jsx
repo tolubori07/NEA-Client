7a0a26581002dfe51f5c104b31a04b9ae582c3fc
Jaabir Adeniji-Bello <tolubori07@gmail.com>
2025-04-27 16:26:22 +0100

    Pish

--- src/pages/Donors/Appointments/SearchAppointment.jsx
+++ src/pages/Donors/Appointments/SearchAppointment.jsx
@@ -27,60 +27,56 @@ const SearchAppointment = () => {
   useEffect(() => {
     fetchCentres();
   }, []);
-  // Only show loading component when actually loading data
-  if (loading) {
-    return <Loading />;
-  }
 
   useEffect(() => {
     if (!isAuthenticated || !user) navigate("/dlogin");
     if (isAuthenticated && user && user.id.startsWith("V")) {
-    navigate("/volunteer/dashboard");
-  }
-}, []);
+      navigate("/volunteer/dashboard");
+    }
+  }, []);
 
-return (
-  <div className="w-full">
-    <Header />
-    <h1 className="text-text font-heading text-3xl ml-12 my-12">
-      Find a donation centre near you...
-    </h1>
-    <div className="results mt-12">
-      <div className="flex justify-center flex-wrap gap-12">
-        {centres.map((centre, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-dark w-full max-w-lg border-2 border-black text-center rounded-lg p-5"
-          >
-            <Link to={`/donor/book/${centre.ID}`}>
-              <h1 className="text-main text-2xl font-display cursor-pointer">
-                {centre.Name}
+  return (
+    <div className="w-full">
+      <Header />
+      <h1 className="text-text font-heading text-3xl ml-12 my-12">
+        Find a donation centre near you...
+      </h1>
+      <div className="results mt-12">
+        <div className="flex justify-center flex-wrap gap-12">
+          {centres.map((centre, index) => (
+            <div
+              key={index}
+              className="bg-white shadow-dark w-full max-w-lg border-2 border-black text-center rounded-lg p-5"
+            >
+              <Link to={`/donor/book/${centre.ID}`}>
+                <h1 className="text-main text-2xl font-display cursor-pointer">
+                  {centre.Name}
+                </h1>
+                <h1 className="text-main text-2xl font-display cursor-pointer">
+                  <a href={`tel:${centre.Phone}`}>{centre.Phone}</a>
+                </h1>
+              </Link>
+              <h2 className="text-text text-lg font-body">
+                {centre.Address}, {centre.City}, {centre.Postcode}
+              </h2>
+              <hr className="h-px my-2 bg-gray-200 border-0" />
+              <h1 className="text-main text-2xl font-heading">
+                <a
+                  href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                    centre.Name,
+                  )}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Click to view on maps
+                </a>
               </h1>
-              <h1 className="text-main text-2xl font-display cursor-pointer">
-                <a href={`tel:${centre.Phone}`}>{centre.Phone}</a>
-              </h1>
-            </Link>
-            <h2 className="text-text text-lg font-body">
-              {centre.Address}, {centre.City}, {centre.Postcode}
-            </h2>
-            <hr className="h-px my-2 bg-gray-200 border-0" />
-            <h1 className="text-main text-2xl font-heading">
-              <a
-                href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                  centre.Name,
-                )}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Click to view on maps
-              </a>
-            </h1>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default SearchAppointment;
